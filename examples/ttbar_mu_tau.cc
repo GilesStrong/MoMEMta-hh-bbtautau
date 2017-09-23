decfdec8dbb6a1b9e8315af2cb18f7bec854067c
GilesStrong <giles.strong@outlook.com>
2017-09-23 18:13:58 +0100

    Updating to larger dataset

--- examples/ttbar_mu_tau.cc
+++ examples/ttbar_mu_tau.cc
@@ -131,7 +131,7 @@ int main(int argc, char** argv) {
     Long64_t nentries = T->GetEntries();
     LorentzVector v_bjet0, v_bjet1, v_tau0, v_tau1;
     if ((options["-e"] != "-1") & (options["-e"] <= nentries)) {
-        std::cout << options["-e"] != "-1" << "\n";
+        std::cout << options["-e"] << "\n";
         std::cout << "Editting stop point\n";
         nentries = stringToDouble(options["-e"]);
     }
